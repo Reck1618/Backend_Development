709270cd1482420cddc59004a8b344756edfab37
Ritik Tyagi <ritiktyagi1.618@gmail.com>
2023-05-08 23:51:30 +0530

    views.py and urls.py

--- Django/app_one/views.py
+++ Django/app_one/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    content = "<html><body><h1>Welcome to my Home Page</h1></body></html>"
+    return HttpResponse(content)
