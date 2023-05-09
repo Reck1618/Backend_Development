d53a6a21c5c249f904efe244b79e077828a7cc7c
Reck1618 <ritiktyagi1.618@gmail.com>
2023-05-09 16:21:18 +0530

    Djangno request module

--- Django/app_one/views.py
+++ Django/app_one/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 def home(request):
     content = "<html><body><h1>Welcome to my Home Page</h1></body></html>"
     return HttpResponse(content)
+
+def demo(request):
+    path = request.path
+    method = request.method
+    content = f"<html><body><h1>Welcome to my Demo Page</h1><p>Path: {path}</p><p>Method: {method}</p></body></html>"
+    return HttpResponse(content)
