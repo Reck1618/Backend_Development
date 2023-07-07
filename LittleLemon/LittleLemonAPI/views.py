baaf5328c3d3afe929ffe1c704893267a8a41cf5
Reck1618 <ritiktyagi1.618@gmail.com>
2023-07-07 19:47:06 +0530

    api's

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET','POST'])
+def books(request):
+    if request.method == 'GET':
+        return Response('list of the books', status=status.HTTP_200_OK)
+    else:
+        return Response('take the shit', status=status.HTTP_200_OK) 
