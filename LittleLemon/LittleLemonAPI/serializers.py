001229009ce740e82fb50813269bef115a6ee686
Ritik <ritiktyagi1.618@gmail.com>
2023-08-21 07:36:25 +0530

    data validation

--- LittleLemon/LittleLemonAPI/serializers.py
+++ LittleLemon/LittleLemonAPI/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import PlayerInfo, Team
+from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
 
 # class PlayerInfoSerializer(serializers.Serializer):
 #     id = serializers.IntegerField()
@@ -13,7 +14,7 @@ class TeamSerializer(serializers.ModelSerializer):
 
 class PlayerInfoSerializer(serializers.ModelSerializer):
     player_age  = serializers.IntegerField(source='age')
-    player_name = serializers.CharField(source='name')
+    player_name = serializers.CharField(source='name', max_length=255, validators=[UniqueValidator(queryset=PlayerInfo.objects.all())])
     yearly_salary = serializers.SerializerMethodField(method_name='salary')
     team = TeamSerializer(read_only=True)
     team_id = serializers.IntegerField(write_only=True)
@@ -21,7 +22,22 @@ class PlayerInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlayerInfo
         fields = ('id', 'player_name', 'player_age', 'yearly_salary', 'team_id', 'team')
+        validators = [
+            UniqueTogetherValidator(
+                queryset=PlayerInfo.objects.all(),
+                fields=['name', 'age']
+            )
+        ]
         # depth = 1
+
 
     def salary(self, player: PlayerInfo):
         return (100 - player.age) * 1000
+
+    # data validation
+    def validate(self, attrs):
+        if (attrs['age']<18):
+            raise serializers.ValidationError('You are too young to play')
+        if(attrs['age']>100):
+            raise serializers.ValidationError('You are too old to play')
+        return super().validate(attrs)
